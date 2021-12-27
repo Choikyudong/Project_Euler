4ef91e0e6768513ee5025afd051c1d357c02a414
Choikyudong <slgmgm@naver.com>
2021-12-27 23:04:48 +0900

    알고리즘 문제들

---
+++ src/file01/Problem09.java
@@ -0,0 +1,17 @@
+package file01;
+
+/*세 자연수 a, b, c가 피타고라스 정리 a^2 + b^2 = c^2를 만족하면 피타고라스 수라고 부릅니다(여기서 a < b < c ). ^는 제곱을 표시*/
+
+    /*예를 들면 32 + 42 = 9 + 16 = 25 = 52이므로 3, 4, 5는 피타고라스 수입니다.*/
+
+/*a + b + c = 1000 인 피타고라스 수 a, b, c는 한 가지 뿐입니다. 이 때, a × b × c 는 얼마입니까?*/
+public class Problem09 {
+
+    public static void main(String[] args) {
+        pythagoreanTriple();
+    }
+
+    static void pythagoreanTriple() {
+    }
+
+}
